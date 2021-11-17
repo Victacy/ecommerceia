a5f86c899f9c92916535b0f0574be93019cc5809
Victacy <adorkoreunice2@gmail.com>
2021-11-17 17:36:36 +0000

    Login and signup added

--- src/routes/drawer.route.js
+++ src/routes/drawer.route.js
@@ -6,7 +6,9 @@ import Checkout from "../screens/checkout";
 import Home from "../screens/home";
 import Login from "../screens/login.js";
 import Details from "../screens/details";
+import Store from "../screens/store";
 import Settings from "../screens/settings";
+import SignUp from "../screens/signup";
 
 const Drawer = createDrawerNavigator();
 
@@ -14,10 +16,12 @@ export default function DrawerRoute() {
   return (
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Login" component={Login} />
+      <Drawer.Screen name="Store" component={Store} />
       <Drawer.Screen name="Cart" component={Cart} />
-      <Drawer.Screen name="Checkout" component={Checkout} />
+      {/* <Drawer.Screen name="Checkout" component={Checkout} /> */}
       <Drawer.Screen name="Product Details" component={Details} />
+      <Drawer.Screen name="Signup" component={SignUp} />
+      <Drawer.Screen name="Login" component={Login} />
       <Drawer.Screen name="Settings" component={Settings} />
     </Drawer.Navigator>
   );
