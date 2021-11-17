0819d4312866019ea5695b2944a3b92d6086405f
Xornam Adorkor <74544333+Victacy@users.noreply.github.com>
2021-11-17 01:55:20 +0000

    Add files via upload

--- src/routes/drawer.route.js
+++ src/routes/drawer.route.js
@@ -1,27 +1,24 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import React from "react";
-import { View, Text } from "react-native";
+// import { View, Text } from "react-native";
 import Cart from "../screens/cart";
 import Checkout from "../screens/checkout";
 import Home from "../screens/home";
-import Login from "../screens/login";
-import ProDetails from "../screens/proDetails";
-import ProStore from "../screens/proStore";
-import SignUp from "../screens/signup";
+import Login from "../screens/login.js";
+import Details from "../screens/details";
+import Settings from "../screens/settings";
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerRoute() {
   return (
-    <Drawer.Navigator initialRouteName="Product Store">
+    <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Login" component={Login} />
       <Drawer.Screen name="Cart" component={Cart} />
       <Drawer.Screen name="Checkout" component={Checkout} />
-      <Drawer.Screen name="Product Details" component={ProDetails} />
-      <Drawer.Screen name="Product Store" component={ProStore} />
-      <Drawer.Screen name="Signup" component={SignUp} />
-      {/* <Drawer.Screen name="Account" component={AccountRoute} /> */}
+      <Drawer.Screen name="Product Details" component={Details} />
+      <Drawer.Screen name="Settings" component={Settings} />
     </Drawer.Navigator>
   );
 }
